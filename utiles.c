9d03ad347dcaf69bc816ba0881784164fb623a00
Trice97 <bolinp@gmail.com>
2025-04-23 16:44:06 +0200

    utiles.c-simple_shell

--- utiles.c
+++ utiles.c
@@ -1,49 +1,55 @@
 #include "main.h"
 
 /**
-* read_command - Affiche un prompt et lit la commande
-* @buffer: zone mémoire où stocker la commande
-* Return: pointeur vers buffer ou NULL en cas d'erreur
-*/
+ * read_command - Affiche un prompt et lit la commande de l'utilisateur
+ * @buffer: Zone mémoire où stocker la commande
+ * Return:Pointeur vers buffer contenant commande ou NULL si cas d'erreur/EOF
+ */
 char *read_command(char *buffer)
 {
-printf("#cisfun$ ");
-fflush(stdout);
+	printf("#cisfun$ ");
+	fflush(stdout);
 
-if (fgets(buffer, BUFFER_SIZE, stdin) == NULL)
-{
-	perror("fgets");
-	return (NULL);
-}
+	if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) /*Gestion d'EOF (CTRL + D)*/
+	{
+		return (NULL);
+	}
 
-buffer[strcspn(buffer, "\n")] = 0;
-return (buffer);
+	buffer[strcspn(buffer, "\n")] = '\0'; /*supprime le saut de ligne*/
+	return (buffer);
 }
 
 /**
-* execute_command - Crée un processus enfant pour exécuter la commande
-* @commande: chemin complet de la commande à exécuter
-*/
+ * execute_command - Crée un processus enfant pour exécuter une commande
+ * @commande: Chemin complet de la commande à exécuter
+ */
 void execute_command(char *commande)
 {
-pid_t pid;
-int status;
+	pid_t pid;
+	int status;
 
-pid = fork();
-if (pid == -1)
-{
-	perror("fork");
-}
-if (pid == 0)
-{
-	char *argv[] = {commande, NULL};
+	if (!commande || *commande == '\0') /*gestion des commandes vides*/
+		return;
 
-	execve(commande, argv, NULL);
-	perror("execve");
-	_exit(1);
-}
-else
-{
-	wait(&status);
-}
+	pid = fork();
+	if (pid == -1) /*erreur de creation de processus*/
+	{
+		perror("fork");
+		return;
+	}
+
+	if (pid == 0) /*processus enfant*/
+	{
+		char *argv[] = {commande, NULL};
+
+		if (execve(commande, argv, environ) == -1) /*excecution de la commande*/
+		{
+			perror("./shell"); /*affiche une erreursi la commande echoue*/
+			_exit(EXIT_FAILURE);
+		}
+	}
+	else/*process parent*/
+	{
+		wait(&status);/*attend la fin de 'execution de l'enfant*/
+	}
 }
