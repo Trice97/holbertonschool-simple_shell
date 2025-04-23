f267f4f7b04fd9135f790da155e0181d52ca656e
Trice97 <bolinp@gmail.com>
2025-04-23 15:45:04 +0200

    main.c-simple_shell/corrigé

--- main.c
+++ main.c
@@ -1,39 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
-* main - Point d'entrée du programme de test
-* Ce programme appelle plusieurs fonctions liées à la gestion
-* de l'environnement : affichage des répertoires PATH, construction
-* d'une liste chaînée à partir de PATH, ajout et suppression de
-* variables d'environnement.
-* Return: Always 0.
-*/
-int main(void)
+ * main - Point d'entrée du programme
+ * @argc: Nombre d'arguments
+ * @argv: Tableau des arguments
+ * @env: Environnement
+ *
+ * Return: Toujours 0
+ */
+int main(int argc, char **argv, char **env)
 {
-path_node *list, *tmp;
-
-printf("===== Test de print_path_dirs =====\n");
-print_path_dirs();
-
-printf("\n===== Test de build_path_list =====\n");
-list = build_path_list();
-tmp = list;
-while (tmp)
-{
-printf("%s\n", tmp->dir);
-tmp = tmp->next;
-}
-free_path_list(list);
-
-printf("\n===== Test de _setenv =====\n");
-_setenv("TEST_VAR", "Hello", 1);
-printf("TEST_VAR = %s\n", getenv("TEST_VAR"));
-
-printf("\n===== Test de _unsetenv =====\n");
-_unsetenv("TEST_VAR");
-printf("TEST_VAR après unset = %s\n", getenv("TEST_VAR"));
-
-return (0);
+	return (simple_shell(argc, argv, env));
 }
