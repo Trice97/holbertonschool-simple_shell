0e5de3a4a1e2cc3e052863ac92afc57abf8f4b0c
Trice97 <bolinp@gmail.com>
2025-04-23 15:53:20 +0200

    main.h-simple_shell

--- main.h
+++ main.h
@@ -1,14 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-
+#include <unistd.h>
+#include <stdio.h>
 
 #define DELIM " \t\r\n"
 #define BUFFER_SIZE 1024
@@ -16,24 +15,28 @@
 extern char **environ;
 
 /**
-* struct path_node - structure pour représenter un répertoire du PATH
-* @dir: chaîne représentant le répertoire
-* @next: pointeur vers le prochain nœud de la liste
-*/
+ * struct path_node - Structure pour représenter un répertoire du PATH
+ * @dir: Chaîne représentant le répertoire
+ * @next: Pointeur vers le prochain nœud de la liste
+ */
 typedef struct path_node
 {
-char *dir;
-struct path_node *next;
+	char *dir;
+	struct path_node *next;
 } path_node;
 
-
-
-
-pid_t getpid(void);
-pid_t getppid(void);
-char **split_line(char *line);
-extern char **environ;
+/* Fonctions principales */
+int simple_shell(int argc, char **argv, char **env);
+void prompt(void);
 char *read_command(char *buffer);
+
+/* Fonctions de gestion des commandes */
+int is_builtin(char *command);
+int execute_builtin(char *command, char **env);
+void execute_command(char *command);
+
+/* Fonctions utilitaires */
+char **split_line(char *line);
 int _which(char *filename);
 char *_getenv(const char *name);
 void print_path_dirs(void);
@@ -42,9 +45,6 @@ void free_path_list(path_node *head);
 int _setenv(const char *name, const char *value, int overwrite);
 int _unsetenv(const char *name);
 void handle_setenv(char *cmd);
-void execute_external(char *cmd);
 void handle_unsetenv(char *cmd);
-int execute_command(char **argv);
 
-
-#endif
+#endif /* MAIN_H */
