fcfa3e44e46fb0dfb1a39a53cf337b2a875f5ac3
Trice97 <bolinp@gmail.com>
2025-04-24 17:16:12 +0200

    correction simple_shell

--- simple_shell.c
+++ simple_shell.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * simple_shell - Fonction principale de l'interpréteur
 * @env: Environnement
@@ -11,32 +10,38 @@ char *line = NULL;
 size_t len = 0;
 ssize_t nread;
 int i;
-
 (void)env;
-
 while (1)
 {
-if (isatty(STDIN_FILENO))
-    printf("#cisfun$ ");
-fflush(stdout);
+	if (isatty(STDIN_FILENO))
+	printf("#cisfun$ ");
+		fflush(stdout);
 
-nread = read_command(&line, &len, STDIN_FILENO);
-if (nread == -1)
-    break;
+	nread = read_command(&line, &len, STDIN_FILENO);
 
-for (i = 0; line[i]; i++)
-{
-    if (line[i] != ' ' && line[i] != '\t')
-	break;
+	if (nread == -1)
+		break;
+
+	for (i = 0; line[i]; i++)
+	{
+		if (line[i] != ' ' && line[i] != '\t')
+		break;
+	}
+
+	if (line[i] == '\0')
+	{
+	free(line);
+	line = NULL;
+	continue;
+	}
+	if (strcmp(line, "exit") == 0)
+	{
+		free(line);
+		break;
+	}
+	execute_command(line);
+	free(line);
+	line = NULL;
 }
-if (line[i] == '\0')
-    continue;
-
-if (strcmp(line, "exit") == 0)
-    break;
-
-execute_command(line);
-}
-free(line);
-return (0);
+	return (0);
 }
